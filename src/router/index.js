8a1e8ed1cb503373340e3b7333ed6efe5ec9f1a4
Lx4 <franck.farcy@gmail.com>
2021-01-26 16:56:26 +0100

    Json server

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Jobs from '../views/jobs/Jobs.vue'
 import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
 
 const routes = [
   {
@@ -25,6 +26,16 @@ const routes = [
     name: 'JobDetails',
     component: JobDetails,
     props: true
+  },
+  {
+    path: '/all-jobs',
+    redirect: 'jobs'
+  },
+  // catchall404
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
